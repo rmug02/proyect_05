8a14107c1f2487504fc0db7f711d73f7b3f52f49
Rafael llanos <mgueavra8@cuc.edu.co>
2024-09-19 09:54:10 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miLogo from "./assets/img/logoReact.png"
+
+function App() {
+
+  return (
+    <>
+     <div>
+        <img src={miLogo} alt="Logo de React" />
+        <h1>Hola mundo</h1>
+        <p>Este es mi primer proyecto en read</p>
+     </div>
+    </>
+  )
+}
+
+export default App
